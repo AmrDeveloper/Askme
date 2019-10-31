d60a4b9579b3ee31946c92df5cdb730655c1c022
AmrDeveloper <amr96@programmer.net>
2019-10-31 03:42:50 +0200

    Use answers and follows routers in Express API

--- Server/app.js
+++ Server/app.js
@@ -4,8 +4,12 @@ const app = express();
 const API_VERSION = "/v1/";
 const userRoutes = require('./api/routes/user');
 const questionRoutes = require('./api/routes/question');
+const answerRoutes = require('./api/routes/answer');
+const followRoutes = require('./api/routes/follow');
 
 app.use(API_VERSION + 'users', userRoutes);
 app.use(API_VERSION + 'questions', questionRoutes);
+app.use(API_VERSION + 'answers', answerRoutes);
+app.use(API_VERSION + 'follows', followRoutes);
 
 module.exports = app;
