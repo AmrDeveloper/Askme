83e75f905c137807f56dfefb5231464250ef4d6c
AmrDeveloper <amr96@programmer.net>
2019-11-03 17:15:52 +0200

    Finish get post reactions

--- Server/api/controllers/react.js
+++ Server/api/controllers/react.js
@@ -5,8 +5,32 @@ const QUERY_DEFAULT_OFFSET = 0;
 const QUERY_DEFAULT_COUNT = 25;
 const QUERY_MAX_COUNT = 50;
 
-exports.createNewReact = (req, res) => {
+exports.getPostReactions = (req, res) => {
+    var offset = req.query.offset;
+    var count = req.query.count;
+    if (offset == null) {
+        offset = QUERY_DEFAULT_OFFSET;
+    }
 
+    if (count == null || count > QUERY_MAX_COUNT) {
+        count = QUERY_DEFAULT_COUNT;
+    }
+
+    const questionID = req.body.questionId;
+    const sqlQuery = 'SELECT * FROM reactions WHEN questionId = ? LIMIT ? OFFSET ?';
+    const args = [
+        questionID,
+        coumt,
+        offset
+    ];
+    database.query(sqlQuery, args, (err, result) => {
+        if (err) throw err;
+        res.status(status.OK).json(result);
+    });
+};
+
+exports.createNewReact = (req, res) => {
+     
 };
 
 exports.deleteReact = (req, res) => {
--- Server/api/routes/react.js
+++ Server/api/routes/react.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/react');
 const checkAuth = require('../../middleware/check_auth');
 const router = express.Router();
 
+router.get('/', checkAuth, controller.getPostReactions)
+
 router.post('/', checkAuth, controller.createNewReact);
 
 router.delete('/', checkAuth, controller.deleteReact);
