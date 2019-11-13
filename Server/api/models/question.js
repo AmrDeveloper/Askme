a0f9221f730ff667218c8e1145cec84d1707a483
AmrDeveloper <amr96@programmer.net>
2019-11-13 18:13:10 +0200

    Move question controller logic to question model

---
+++ Server/api/models/question.js
@@ -0,0 +1,69 @@
+const database = require('../../database/config');
+
+exports.getUserQuestions = args => new Promise((resolve, reject) => {
+    const query = `SELECT DISTINCT id,
+                                   title,
+                                   (SELECT username FROM users WHERE fromUser = users.id) AS fromUser,
+                                   (SELECT avatar FROM users WHERE fromUser = users.id) AS avatar,
+                                   askedDate,
+                                   anonymous
+                                   FROM questions WHERE toUser = ? LIMIT ? OFFSET ?`;
+
+    database.query(query, args, (err, result) => {
+        if (err) throw err;
+        resolve(result);
+    });
+});
+
+exports.getAskedQuestions = args => new Promise((resolve, reject) => {
+    const query = `SELECT DISTINCT id,
+                                   title,
+                                   (SELECT username FROM users WHERE toUser = users.id) AS toUser,
+                                   (SELECT avatar FROM users WHERE toUser = users.id) AS avatar,
+                                   askedDate,
+                                   anonymous
+                                   FROM questions WHERE fromUser = ? LIMIT ? OFFSET ?`;
+
+    database.query(query, args, (err, result) => {
+        if (err) throw err;
+        resolve(result);
+    });
+});
+
+exports.getQuestionByID = args => new Promise((resolve, reject) => {
+    const query = `SELECT DISTINCT title,
+                                   (SELECT username FROM users WHERE toUser = users.id) AS toUser,
+                                   (SELECT username FROM users WHERE fromUser = users.id) AS fromUser,
+                                   anonymous,
+                                   askedDate
+                                   FROM questions WHERE id = ? LIMIT 1`;
+
+    database.query(query, args, (err, result) => {
+        if (err) throw err;
+        if (result.length == 1) {
+            resolve([true, result[0]]);
+        } else {
+            resolve([false]);
+        }
+    });
+});
+
+exports.createNewQuestion = args => new Promise((resolve, reject) => {
+    const query = `INSERT INTO questions (title, toUser, fromUser, anonymous, askedDate) VALUES (?, ?, ?, ?, ?)`;
+
+    database.query(query, args, (err, result) => {
+        if(err) throw err;
+        const isValidRequest = result['affectedRows'] == 1;
+        resolve(isValidRequest);
+    });
+});
+
+exports.deleteQuestion = args => new Promise((resolve, reject) => {
+    const query = `DELETE FROM questions WHERE id = ?`;
+
+    database.query(query, args, (err, result) => {
+        if (err) throw err;
+        const isValidRequest = result['affectedRows'] == 1;
+        resolve(isValidRequest);
+    });
+});
