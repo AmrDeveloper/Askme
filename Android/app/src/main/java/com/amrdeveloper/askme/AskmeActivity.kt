df9acc212dc6c3d64e63682462afb24e2dcf611a
AmrDeveloper <amr96@programmer.net>
2019-12-11 12:53:30 +0200

    Create AskmeActivity class to make easy to manage network state and event

--- Android/app/src/main/java/com/amrdeveloper/askme/AskmeActivity.kt
+++ Android/app/src/main/java/com/amrdeveloper/askme/AskmeActivity.kt
@@ -1,6 +1,12 @@
 package com.amrdeveloper.askme
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.amrdeveloper.askme.utils.NetworkReceiver
+import com.amrdeveloper.askme.utils.NetworkStateListener
 import org.greenrobot.eventbus.EventBus
 
 abstract class AskmeActivity : AppCompatActivity() {
@@ -9,18 +15,37 @@ abstract class AskmeActivity : AppCompatActivity() {
     abstract fun onNetworkOff()
     abstract fun onThemeChanged()
 
+    private lateinit var mNetworkReceiver : NetworkReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        mNetworkReceiver = NetworkReceiver(mNetworkChangeListener)
+    }
+
     override fun onStart() {
         super.onStart()
+        registerReceiver(mNetworkReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
         EventBus.getDefault().register(this)
     }
 
     override fun onPause() {
         super.onPause()
+        unregisterReceiver(mNetworkReceiver)
         EventBus.getDefault().unregister(this)
     }
 
     override fun onStop() {
         super.onStop()
         EventBus.getDefault().unregister(this)
+    }
+
+    private val mNetworkChangeListener : NetworkStateListener = object : NetworkStateListener{
+        override fun onNetworkConnected() {
+            onNetworkOn()
+        }
+
+        override fun onNetworkDisconnected() {
+            onNetworkOff()
+        }
     }
 }
