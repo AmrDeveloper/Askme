881f7d985a1deae91944f6c8c3f6961458f41aa3
AmrDeveloper <amr96@programmer.net>
2019-12-11 13:22:31 +0200

    Create basic desin for login activity

--- Android/app/src/main/java/com/amrdeveloper/askme/views/LoginActivity.kt
+++ Android/app/src/main/java/com/amrdeveloper/askme/views/LoginActivity.kt
@@ -3,13 +3,13 @@ package com.amrdeveloper.askme.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.amrdeveloper.askme.contracts.LoginContract
 import com.amrdeveloper.askme.presenters.LoginPresenter
 import com.amrdeveloper.askme.R
+import com.amrdeveloper.askme.databinding.ActivityLoginBinding
 import com.amrdeveloper.askme.utils.Session
 import com.amrdeveloper.askme.events.LoginFailureEvent
 import com.amrdeveloper.askme.events.LoginSuccessEvent
@@ -19,31 +19,21 @@ import org.greenrobot.eventbus.EventBus
 
 class LoginActivity : AppCompatActivity(), LoginContract.View {
 
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEdit: EditText
-    private lateinit var loginButton: Button
-
+    private lateinit var mLoginActivity: ActivityLoginBinding
     private lateinit var mLoginPresenter: LoginPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        initViews()
-
+        mLoginActivity = DataBindingUtil.setContentView(this, R.layout.activity_login)
         mLoginPresenter = LoginPresenter(this)
 
+        /*
         loginButton.setOnClickListener { view ->
             val email: String = emailEditText.text.toString()
             val password: String = passwordEdit.text.toString()
             mLoginPresenter.makeLoginRequest(email, password)
         }
-    }
-
-    fun initViews() {
-        emailEditText = findViewById(R.id.emailEdit)
-        passwordEdit = findViewById(R.id.passwordEdit)
-        loginButton = findViewById(R.id.loginButton)
+         */
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -57,7 +47,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onLoginFailureEvent(event: LoginFailureEvent) {
-         Toast.makeText(applicationContext, "Invalid Login", Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "Invalid Login", Toast.LENGTH_SHORT).show()
     }
 
     override fun showProgressBar() {
