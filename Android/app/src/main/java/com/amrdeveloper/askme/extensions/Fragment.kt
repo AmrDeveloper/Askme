39efd8284b7ae7d13bf77d6807d854d336cdafe0
AmrDeveloper <amr96@programmer.net>
2019-12-13 14:22:23 +0200

    Create fragment backstack with fragment class name

--- Android/app/src/main/java/com/amrdeveloper/askme/extensions/Fragment.kt
+++ Android/app/src/main/java/com/amrdeveloper/askme/extensions/Fragment.kt
@@ -6,6 +6,6 @@ import androidx.fragment.app.FragmentManager
 fun FragmentManager.openFragmentInto(container : Int, fragment: Fragment){
     this.beginTransaction()
         .replace(container, fragment)
+        .addToBackStack(fragment.javaClass.name)
         .commit()
-
 }
