aa2939819f15fe343d63ac8869bb7ddd4f65d0d9
AmrDeveloper <amr96@programmer.net>
2019-12-14 10:00:12 +0200

    Update feed router

--- Server/api/models/feed.js
+++ Server/api/models/feed.js
@@ -4,9 +4,13 @@ exports.getUserFeed = (args) => new Promise((resolve, reject) => {
     const query = `SELECT DISTINCT answers.id, 
                                    (SELECT title FROM questions WHERE questionId = questions.id) AS questionBody,
                                    answers.body AS answerBody,
-                                   (SELECT username FROM users WHERE toUser= users.id) AS toUsername,
+                                   (SELECT username FROM users WHERE fromUser = users.id) AS fromUsername,
+                                   (SELECT email FROM users WHERE fromUser = users.id) AS fromUserEmail,
                                    (SELECT avatar FROM users WHERE fromUser = users.id) AS fromUserAvatar,
-                                   (SELECT avatar FROM users WHERE toUser= users.id) AS toUserAvatar,
+                                   (SELECT username FROM users WHERE toUser = users.id) AS toUsername,
+                                   (SELECT email FROM users WHERE toUser = users.id) AS toUserEmail,
+                                   (SELECT avatar FROM users WHERE toUser = users.id) AS toUserAvatar,
+                                   (SELECT COUNT(*) FROM reactions WHERE answerId = answers.id) AS reactions,
                                    answers.answerdDate
                     FROM answers WHERE fromUser = ? LIMIT ? OFFSET ?`;
     database.query(query, args, (err, result) => {
