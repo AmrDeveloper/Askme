b7cf479254e3084170eae2d7109a539b6487439d
AmrDeveloper <amr96@programmer.net>
2019-12-14 10:02:23 +0200

    Get more info from Feed Get(/) frouter

--- Server/api/models/feed.js
+++ Server/api/models/feed.js
@@ -1,12 +1,14 @@
 const database = require('../../database/config');
 
 exports.getUserFeed = (args) => new Promise((resolve, reject) => {
-    const query = `SELECT DISTINCT answers.id, 
+    const query = `SELECT DISTINCT answers.id AS answerId, 
                                    (SELECT title FROM questions WHERE questionId = questions.id) AS questionBody,
                                    answers.body AS answerBody,
+                                   (SELECT id FROM users WHERE fromUser = users.id) AS fromUserId,
                                    (SELECT username FROM users WHERE fromUser = users.id) AS fromUsername,
                                    (SELECT email FROM users WHERE fromUser = users.id) AS fromUserEmail,
                                    (SELECT avatar FROM users WHERE fromUser = users.id) AS fromUserAvatar,
+                                   (SELECT id FROM users WHERE toUser = users.id) AS toUserId,
                                    (SELECT username FROM users WHERE toUser = users.id) AS toUsername,
                                    (SELECT email FROM users WHERE toUser = users.id) AS toUserEmail,
                                    (SELECT avatar FROM users WHERE toUser = users.id) AS toUserAvatar,
